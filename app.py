f505f5f708efa4dad99353bedd21c40c4077d017
Bulray <97762160+Bulray@users.noreply.github.com>
2022-05-07 22:10:16 +0300

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -8,10 +9,47 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
+def build_query(it, cmd, value):
+    res = map(lambda v: v.strip(), it)
+    if cmd == "filter":
+        res = filter(lambda v, txt=value: txt in v, res)
+    if cmd == "map":
+        arg = int(value)
+        res = map(lambda v, idx=arg: v.split(" ")[idx], res)
+    if cmd == "unique":
+        res = set(res)
+    if cmd == "sort":
+        reverse = value == "desc"
+        res = sorted(res, reverse=reverse)
+    if cmd == "limit":
+        arg = int(value)
+        res = list(res)[:arg]
+    return res
+
+
 @app.route("/perform_query")
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    try:
+        cmd1 = request.args["cmd1"]
+        cmd2 = request.args["cmd2"]
+        value1 = request.args["value1"]
+
+        value2 =request.args["value2"]
+        file_name = request.args["file_name"]
+    except KeyError:
+        raise BadRequest
+
+    file_path = os.path.join(DATA_DIR, file_name)
+    if not os.path.exists(file_path):
+        return BadRequest(description=f"{file_name} was not found")
+
+
+    with open(file_path) as fd:
+        res = build_query(fd, cmd1, value1)
+        res = build_query(res, cmd2, value2)
+        content = '\n'.join(res)
+        print(content)
+    return app.response_class(content, content_type="text/plain")
+
+if __name__ == '__main__':
+    app.run(debug=True)

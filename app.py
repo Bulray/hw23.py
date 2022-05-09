d90dbf9dc2162e5e14dcc79422058e0b540cbccf
Bulray <97762160+Bulray@users.noreply.github.com>
2022-05-10 00:45:33 +0300

    second commit

--- app.py
+++ app.py
@@ -30,11 +30,10 @@ def build_query(it, cmd, value):
 @app.route("/perform_query")
 def perform_query():
     try:
-        cmd1 = request.args["cmd1"]
+        cmd1 = request.args.get("cmd1")
         cmd2 = request.args["cmd2"]
-        value1 = request.args["value1"]
-
-        value2 =request.args["value2"]
+        value1 = request.args.get("value1")
+        value2 = request.args["value2"]
         file_name = request.args["file_name"]
     except KeyError:
         raise BadRequest
@@ -43,13 +42,15 @@ def perform_query():
     if not os.path.exists(file_path):
         return BadRequest(description=f"{file_name} was not found")
 
-
     with open(file_path) as fd:
+
+
         res = build_query(fd, cmd1, value1)
         res = build_query(res, cmd2, value2)
         content = '\n'.join(res)
         print(content)
     return app.response_class(content, content_type="text/plain")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
